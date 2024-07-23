e0116772a48e9c0c8ba95796d38d4f90e3ae4420
Mohammad-Ali-innostax <mohammad.ali@innostax.com>
2024-07-23 10:42:36 +0530

    Standardised the endpoints to send response and receive parameters in the List of Objects format only

--- API/app/main.py
+++ API/app/main.py
@@ -1,46 +1,52 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from . import database, schemas
+from typing import List
 
 app = FastAPI()
 
-@app.get("/items/", response_model=list[schemas.Item])
+@app.get("/items/", response_model=List[schemas.Item])
 def read_items(db:Session = Depends(database.get_db)):
     return db.query(database.Item)
 
-@app.get("/items/{item_id}", response_model=schemas.Item)
+@app.get("/items/{item_id}", response_model=List[schemas.Item])
 def read_item(item_id:int, db:Session = Depends(database.get_db)):
     db_item = db.query(database.Item).filter(database.Item.id == item_id).first()
     if db_item == None:
         raise HTTPException(status_code=404, detail="Item not found")
-    return db_item
+    return [db_item]
 
-@app.post("/items/", response_model=schemas.Item )
-def create_item(item: schemas.ItemCreate, db:Session = Depends(database.get_db)):
-    new_item = database.Item(item_name= item.item_name, brand_name= item.brand_name, price = item.price)
-    db.add(new_item)
-    db.commit()
-    db.refresh(new_item)
-    return new_item
+@app.post("/items/", response_model=List[schemas.Item] )
+def create_item(items: List[schemas.ItemCreate], db:Session = Depends(database.get_db)):
+    db_items = []
+    for item in items:
+        new_item = database.Item(item_name= item.item_name, brand_name= item.brand_name, price = item.price)
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+        db_items.append(new_item)
+    return db_items
 
-@app.put("/items/{item_id}", response_model=schemas.Item)
-def update_item(item_id:int, item: schemas.ItemUpdate, db:Session = Depends(database.get_db)):
-    old_item = read_item(item_id, db)
+@app.put("/items/{item_id}", response_model=List[schemas.Item])
+def update_item(item_id:int, item: List[schemas.ItemUpdate], db:Session = Depends(database.get_db)):
+    old_item = read_item(item_id, db)[0]  #getting the first item
+    # print(old_item)
+    # print(item)
     if old_item == None:
         raise HTTPException(status_code=404, detail="Item not found")
-    old_item.item_name = item.item_name
-    old_item.brand_name = item.brand_name
-    old_item.price = item.price
+    old_item.item_name = item[0].item_name 
+    old_item.brand_name = item[0].brand_name
+    old_item.price = item[0].price
     
     db.commit()
     db.refresh(old_item)
-    return old_item
+    return [old_item]
 
-@app.delete("/items/{item_id}", response_model=schemas.Item)
+@app.delete("/items/{item_id}", response_model=List[schemas.Item])
 def delete_user(item_id:int, db:Session = Depends(database.get_db)):
-    db_item = read_item(item_id, db)
+    db_item = read_item(item_id, db)[0]
     if db_item == None:
         raise HTTPException(status_code=404, detail="Item not found")
     db.delete(db_item)
     db.commit()
-    return db_item
+    return [db_item]
